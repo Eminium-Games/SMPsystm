1cc90c58ce7b4b46c6908babb1664e5b0a89f156
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-02-16 20:10:09 +0100

    Refactor player function execution to group by world and manage game rules in SMPPositionSaver

--- src/main/java/fr/smp/smpositionsaver/SMPPositionSaver.java
+++ src/main/java/fr/smp/smpositionsaver/SMPPositionSaver.java
@@ -1,6 +1,13 @@
 package fr.smp.smpositionsaver;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -28,23 +35,63 @@ public class SMPPositionSaver extends JavaPlugin {
         // Scheduler: run per-player functions
         // Every tick: execute function bracken:player/tick as each player in SMP worlds
         getServer().getScheduler().runTaskTimer(this, () -> {
+            // Grouper les joueurs par monde pour minimiser les changements de gamerules
+            Map<World, List<Player>> byWorld = new HashMap<>();
             for (Player player : getServer().getOnlinePlayers()) {
-                String world = player.getWorld().getName();
-                if (getConfigManager().isSmpWorld(world)) {
+                String worldName = player.getWorld().getName();
+                if (getConfigManager().isSmpWorld(worldName)) {
+                    World w = player.getWorld();
+                    byWorld.computeIfAbsent(w, k -> new ArrayList<>()).add(player);
+                }
+            }
+
+            for (Map.Entry<World, List<Player>> entry : byWorld.entrySet()) {
+                World world = entry.getKey();
+                List<Player> players = entry.getValue();
+
+                Boolean prevSend = world.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK);
+                Boolean prevLog = world.getGameRuleValue(GameRule.LOG_ADMIN_COMMANDS);
+                world.setGameRule(GameRule.SEND_COMMAND_FEEDBACK, false);
+                world.setGameRule(GameRule.LOG_ADMIN_COMMANDS, false);
+
+                for (Player player : players) {
                     Bukkit.dispatchCommand(getServer().getConsoleSender(), "execute as " + player.getName() + " run function bracken:player/tick");
                 }
+
+                world.setGameRule(GameRule.SEND_COMMAND_FEEDBACK, prevSend != null ? prevSend : true);
+                world.setGameRule(GameRule.LOG_ADMIN_COMMANDS, prevLog != null ? prevLog : true);
             }
         }, 0L, 1L);
 
         // Every 10 ticks: apply or remove species attributes for each player
         getServer().getScheduler().runTaskTimer(this, () -> {
+            // Grouper par monde afin de désactiver/rétablir les gamerules par monde
+            Map<World, List<Player>> byWorld = new HashMap<>();
             for (Player player : getServer().getOnlinePlayers()) {
-                String world = player.getWorld().getName();
-                if (getConfigManager().isSmpWorld(world)) {
-                    Bukkit.dispatchCommand(getServer().getConsoleSender(), "execute as " + player.getName() + " run function bracken:player/attributes/apply_species");
-                } else {
-                    Bukkit.dispatchCommand(getServer().getConsoleSender(), "execute as " + player.getName() + " run function bracken:player/attributes/remove_all");
+                World w = player.getWorld();
+                byWorld.computeIfAbsent(w, k -> new ArrayList<>()).add(player);
+            }
+
+            for (Map.Entry<World, List<Player>> entry : byWorld.entrySet()) {
+                World world = entry.getKey();
+                List<Player> players = entry.getValue();
+
+                Boolean prevSend = world.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK);
+                Boolean prevLog = world.getGameRuleValue(GameRule.LOG_ADMIN_COMMANDS);
+                world.setGameRule(GameRule.SEND_COMMAND_FEEDBACK, false);
+                world.setGameRule(GameRule.LOG_ADMIN_COMMANDS, false);
+
+                boolean isSmp = getConfigManager().isSmpWorld(world.getName());
+                for (Player player : players) {
+                    if (isSmp) {
+                        Bukkit.dispatchCommand(getServer().getConsoleSender(), "execute as " + player.getName() + " run function bracken:player/attributes/apply_species");
+                    } else {
+                        Bukkit.dispatchCommand(getServer().getConsoleSender(), "execute as " + player.getName() + " run function bracken:player/attributes/remove_all");
+                    }
                 }
+
+                world.setGameRule(GameRule.SEND_COMMAND_FEEDBACK, prevSend != null ? prevSend : true);
+                world.setGameRule(GameRule.LOG_ADMIN_COMMANDS, prevLog != null ? prevLog : true);
             }
         }, 0L, 10L);
         
