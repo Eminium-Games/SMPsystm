2042c4637da1068cfdb86fc6632c9e854964c3d6
fox3000foxy <fox3000foxy@gmail.com>
2026-02-23 17:03:40 +0100

    Remove unused import and clean up PlayerWorldChangeListener

--- src/main/java/fr/smp/smpositionsaver/listeners/PlayerWorldChangeListener.java
+++ src/main/java/fr/smp/smpositionsaver/listeners/PlayerWorldChangeListener.java
@@ -1,12 +1,13 @@
 package fr.smp.smpositionsaver.listeners;
 
+import java.net.http.WebSocket.Listener;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import fr.smp.smpositionsaver.SMPPositionSaver;
